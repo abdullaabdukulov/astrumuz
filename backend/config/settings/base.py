a33eadf47bd0f074e861e895c2bd26929a0cab20
Abdulla <abduqulovabdulla3108@gmail.com>
2025-05-17 13:31:19 +0500

    fixed

--- backend/config/settings/base.py
+++ backend/config/settings/base.py
@@ -23,7 +23,6 @@ BASE_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "django.contrib.gis",
     "django.contrib.postgres",
 ]
 THIRD_PARTY_APPS = [
@@ -76,8 +75,12 @@ WSGI_APPLICATION = "config.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.environ.get("DB_NAME"),
+        "USER": os.environ.get("DB_USER"),
+        "PASSWORD": os.environ.get("DB_PASSWORD"),
+        "HOST": os.environ.get("DB_HOST", "localhost"),
+        "PORT": os.environ.get("DB_PORT", "5432"),
     }
 }
 
