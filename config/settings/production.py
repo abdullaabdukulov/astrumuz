f70856e9934cad77b20ee35892f5aa692c2b4ccd
Abdulla <abduqulovabdulla3108@gmail.com>
2026-02-19 14:29:26 +0500

    fixed

--- config/settings/production.py
+++ config/settings/production.py
@@ -1,13 +1,21 @@
 from .base import *  # noqa
 
 ALLOWED_HOSTS = [
-    "api.example.com",
-    "admin.example.com" "localhost",
+    "devuser.astrum.uz",
+    "localhost",
     "127.0.0.1",
-    "server_ip_address",
+    "46.101.129.181",
 ]
-CSRF_TRUSTED_ORIGINS = ["https://api.example.com", "https://admin.example.com"]
-CORS_ALLOWED_ORIGINS = ["https://api.example.com", "https://admin.example.com"]
+CORS_ALLOWED_ORIGINS = [
+    "https://astrum.uz",
+    "https://www.astrum.uz",
+]
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://devuser.astrum.uz",
+    "https://astrum.uz",
+    "https://www.astrum.uz",
+]
 DEBUG = False
 CSRF_COOKIE_SECURE = True
 SECURE_HSTS_SECONDS = 60 * 60 * 24 * 7 * 52  # one year
